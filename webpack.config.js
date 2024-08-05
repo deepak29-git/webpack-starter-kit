9a54ae20d8642178bfee8dbff7ed859b1fb20bc4
deepak29-git <dg887180@gmail.com>
2024-08-05 20:33:37 +0530

    webpack-starter-kit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // Add .jsx to resolve extensions
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Caching",
+    }),
+  ],
+  cache: {
+    type: "filesystem",
+    cacheLocation: path.resolve(__dirname, "."),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
